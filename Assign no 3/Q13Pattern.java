5d54aa050a0182e8fb5ccc20599f5891764f95ee
PujaShingare-KHFeb25 <pujashingare496@gmail.com>
2025-02-24 21:14:56 +0530

    Add files via upload

---
+++ Assign no 3/Q13Pattern.java
@@ -0,0 +1,43 @@
+class Q13Pattern
+{
+public static void main(String[] args)
+{
+  for(int i=1;i<=5;i++)
+   {
+    for(int j=1;j<=i;j++)
+    {
+    System.out.print(i);
+
+  if(j<i)
+    {
+      System.out.print("*");
+}}
+System.out.println();
+}
+
+for(int i=5;i>=1;i--)
+   {
+    for(int j=1;j<=i;j++)
+    {
+    System.out.print(i);
+
+  if(j<i)
+    {
+      System.out.print("*");
+}}
+System.out.println();
+}
+}
+}
+/*OUTPUT:
+1
+2*2
+3*3*3
+4*4*4*4
+5*5*5*5*5
+5*5*5*5*5
+4*4*4*4
+3*3*3
+2*2
+1
+*/
